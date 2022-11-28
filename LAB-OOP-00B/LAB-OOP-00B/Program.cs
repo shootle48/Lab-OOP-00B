ec7dfdbe0eaf022f331b0021131c4291f38fce62
B-Man <lolicon2544@gmail.com>
2022-11-28 16:31:39 +0700

    Add AvgWeight to Program

---
+++ LAB-OOP-00B/LAB-OOP-00B/Program.cs
@@ -0,0 +1,24 @@
+﻿using Lab_OOP_00A;
+
+Student student1 = new Student();
+student1.name = "Mickey";
+student1.weight = 20.8f;
+
+Student student2 = new Student();
+student2.name = "Minnie";
+student2.weight = 18.2f;
+
+Student student3 = new Student();
+student3.name = "Biggie";
+student3.weight = 65.4f;
+
+Student student4 = new Student();
+student4.name = "Mixie";
+student4.weight = 70.2f;
+
+Student student5 = new Student();
+student5.name = "Nettie";
+student5.weight = 67.8f;
+
+float avgweight = (student1.weight + student5.weight + student3.weight + student4.weight + student5.weight) / 5;
+Console.WriteLine(avgweight);
---
+++ LAB-OOP-00B/LAB-OOP-00B/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_OOP_00A
+{
+    internal class Student
+    {
+        public string name;
+        public float weight;
+    }
+}
